f8d1eadbec1b1fa6e4eabeb7c9c3619bd7a9e75e
akatsuki <AkafreakingBox>
2019-03-18 20:06:23 +0200

    update deploy.sh with public url

--- deploytest.sh
+++ deploytest.sh
@@ -1,4 +1,4 @@
 # #!/bin/bash
 sudo rm -rf dist
 # build the /dist for public url 
-sudo parcel build index.html --public-url http://62.78.181.155:10080/CS_cityscopeJS_Scanner/dist
+sudo parcel build index.html --public-url http://62.78.181.155:10080/scanner/dist
