e43f043f89859a2baa7d90f88c9e9c50245f1ba1
Akaboxavi <akafreakingbox>
2019-05-27 01:58:03 +0300

    update sh script . gitignore ˆ

--- deploytest.sh
+++ deploytest.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 # #!/bin/bash
 sudo rm -rf dist
-# build the /dist for public url 
+# build the /dist for public url
 sudo parcel build index.html --public-url http://62.78.181.155:10080/scanner/dist
